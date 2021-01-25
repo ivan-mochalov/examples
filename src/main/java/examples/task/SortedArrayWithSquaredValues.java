bd5fbce0d663bdcbb8addbc9894bb22e3183ddde
Ivan Mochalov <iv.moc@yandex.ru>
2021-01-26 00:16:52 +0300

    added 'sorted array with squared values' task

---
+++ src/main/java/examples/task/SortedArrayWithSquaredValues.java
@@ -0,0 +1,30 @@
+/*
+ * Возвезти в квадрат значения отсортированного массива так, чтобы сортировка сохранилась
+ */
+package examples.task;
+
+import lombok.NonNull;
+
+public class SortedArrayWithSquaredValues {
+
+    public int[] squareUp(@NonNull int[] values) {
+        int[] result = new int[values.length];
+        int leftPointer = 0;
+        int rightPointer = values.length - 1;
+
+        for (int i = result.length - 1; i >= 0; i--) {
+            int leftCurrent = values[leftPointer] * values[leftPointer];
+            int rightCurrent = values[rightPointer] * values[rightPointer];
+
+            if (leftCurrent > rightCurrent) {
+                result[i] = leftCurrent;
+                leftPointer++;
+            } else {
+                result[i] = rightCurrent;
+                rightPointer--;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/examples/task/SortedArrayWithSquaredValuesTest.java
@@ -0,0 +1,30 @@
+package examples.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SortedArrayWithSquaredValuesTest {
+
+    private SortedArrayWithSquaredValues target;
+
+    @BeforeEach
+    void setUp() {
+        target = new SortedArrayWithSquaredValues();
+    }
+
+    @Test
+    void givenSortedArray_whenSquareUpExecuted_thenArrayValuesBecomesSquaredAndSortingStillPresent() {
+        int[] values = new int[]{-5, -3, 1, 2, 4, 6};
+
+        int[] result = target.squareUp(values);
+
+        System.out.println(Arrays.toString(result));
+
+        assertEquals(1, result[0]);
+        assertEquals(36, result[5]);
+    }
+}
